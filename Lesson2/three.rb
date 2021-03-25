5bb703003a634b14334b37e35753188cf12edaa1
badrandom <denis.varihanov@ya.ru>
2021-03-25 11:43:42 +0300

    No code duplication

--- Lesson2/three.rb
+++ Lesson2/three.rb
@@ -2,10 +2,9 @@
 
 puts 'Fibonacci array:'
 fib_array = [0, 1]
-counter = fib_array.last(2).sum
 loop do
-  fib_array << counter
   counter = fib_array.last(2).sum
   break if counter > 100
+  fib_array << counter
 end
 print "#{fib_array}\n"
