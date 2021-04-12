f6f74e3660bfc86d34201af5db3336b47f76fe02
badrandom <denis.varihanov@ya.ru>
2021-04-13 00:05:03 +0300

    Finally done!

--- Lesson5/instance_counter.rb
+++ Lesson5/instance_counter.rb
@@ -1,23 +1,31 @@
 module InstanceCounter
+
   def self.included(base)
     base.extend ClassMethods
     base.include InstanceMethods
   end
 
   module ClassMethods
-    class << self
-      attr_accessor :instances
-    end
 
     def instances
-      @instances
+      self.instance_variable_get(:@instances)
     end
 
   end
 
   module InstanceMethods
+
     def register_instance
-      @instances += 1
+      instances = self.class.instance_variable_get(:@instances)
+      if instances
+        self.class.instance_variable_set(:@instances, instances + 1)
+      else
+        class << self
+          attr_accessor :instances
+        end
+        self.class.instance_variable_set(:@instances, 1)
+      end
     end
+
   end
 end
